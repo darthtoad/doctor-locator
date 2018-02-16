111afdacb31ba19a6420a579712de701d3a946c6
Sam Gespass <30681859+darthtoad@users.noreply.github.com>
2018-02-16 12:17:58 -0800

    search by name works without input

--- js/scripts-interface.js
+++ js/scripts-interface.js
@@ -1,10 +1,52 @@
+import { Search } from "./../js/scripts.js"
+
+// Search.prototype.parseDataForIssue = function(response) {
+//   response.data.forEach(function(doctor){
+//     return null;
+//   });
+// }
+
+Search.prototype.parseDataForName = function(name) {
+  name.then(function(response) {
+    // console.log(response);
+    let body = JSON.parse(response);
+    console.log(body.data);
+    console.log(body.data[0].profile.first_name);
+    console.log(body.data.length);
+    // $('#result').append(`${body.word} `);
+    // body.data.forEach(function(doctor){
+    for (let i = 0; i < body.data.length; i++) {
+      for (let j = 0; j < body.data[i].practices.length; j++) {
+        $("#result").append(body.data[i].practices[j].name);
+        $("#result").append(body.data[i].practices[j].accepts_new_patients);
+        $("#result").append(body.data[i].practices[j].visit_address.city);
+        $("#result").append(body.data[i].practices[j].visit_address.state);
+        $("#result").append(body.data[i].practices[j].visit_address.street);
+        $("#result").append(body.data[i].practices[j].visit_address.zip);
+        $("#result").append(body.data[i].practices[j].website);
+        body.data[i].practices[0].phones.forEach(function(phone){
+          $("#result").append(phone.number + " " + phone.type);
+        })
+      }
+
+    }
+
+    // });
+  }, function(error) {
+   // $('.showErrors').text(`There was an error processing your request: ${error.message}`);
+   return `There was an error processing your request: ${error.message}`;
+  });
+}
+
 $(document).ready(function(){
-  $("#by-issue").submit(function(event){
-    event.preventDefault();
-    let issue = $("#issue").val();
-  })
-  $("#by-name").submit(function(event){
-    event.preventDefault();
-    let name = $("#name").val();
-  })
+  // $("#by-issue").submit(function(event){
+  //   event.preventDefault();
+  //   let issue = $("#issue").val();
+  // })
+  // $("#by-name").submit(function(event){
+  //   event.preventDefault();
+  //   let name = $("#name").val();
+  // })
+  let search = new Search("name", "Christina Peterson", "best-match", 10);
+  search.parseDataForName(search.search());
 })
--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,4 @@
-const apiKey = require('./../env').apiKey;
+const apiKey = require('./../.env').apiKey;
 
 export class Search {
   constructor(type, text, sort, limit) {
@@ -9,38 +9,38 @@ export class Search {
   }
 
   search() {
-    if (this.type === "issue") {
-      let request = new XMLHttpRequest();
-
-      let issue = new Promise(function(resolve, reject) {
-        let request = new XMLHttpRequest();
-        let url = `https://api.betterdoctor.com/2016-03-01/doctors?query=headache&location=45.555%2C-122.683%2C20&user_location=45.555%2C-122.683&sort=best-match-asc&skip=0&limit=10&user_key=${apiKey}`;
-        request.onload = function() {
-          if (this.status === 200) {
-            resolve(request.response);
-          } else {
-            reject(Error(request.statusText));
-          }
-        }
-        request.open("GET", url, true);
-        request.send();
-      });
-
-      issue.then(function(response) {
-        let body = JSON.parse(response);
-        // $('#result').append(`${body.word} `);
-        return response;
-      }, function(error) {
-       // $('.showErrors').text(`There was an error processing your request: ${error.message}`);
-       return `There was an error processing your request: ${error.message}`;
-      });
-    } else if (this.type === "name") {
+    // if (this.type === "issue") {
+    //   let request = new XMLHttpRequest();
+    //
+    //   let issue = new Promise(function(resolve, reject) {
+    //     let request = new XMLHttpRequest();
+    //     let url = `https://api.betterdoctor.com/2016-03-01/doctors?query=headache&location=45.555%2C-122.683%2C20&user_location=45.555%2C-122.683&sort=best-match-asc&skip=0&limit=10&user_key=${apiKey}`;
+    //     request.onload = function() {
+    //       if (this.status === 200) {
+    //         resolve(request.response);
+    //       } else {
+    //         reject(Error(request.statusText));
+    //       }
+    //     }
+    //     request.open("GET", url, true);
+    //     request.send();
+    //   });
+    //
+    //   issue.then(function(response) {
+    //     let body = JSON.parse(response);
+    //     // $('#result').append(`${body.word} `);
+    //     return parseDataForIssue(response);
+    //   }, function(error) {
+    //    // $('.showErrors').text(`There was an error processing your request: ${error.message}`);
+    //    return `There was an error processing your request: ${error.message}`;
+    //   });
+    /*} else*/ if (this.type === "name") {
       let request = new XMLHttpRequest();
 
       let name = new Promise(function(resolve, reject) {
         let request = new XMLHttpRequest();
-        let url = `https://api.betterdoctor.com/2016-03-01/doctors?name=Joop%20Offerman&location=40.440%2C-79.995%2C20&user_location=40.440%2C-79.995&sort=best-match-asc&skip=0&limit=10&user_key=
-      ${apiKey}`;
+        let url = `https://api.betterdoctor.com/2016-03-01/doctors?name=Joop%20Offerman&location=40.440%2C-79.995%2C20&user_location=40.440%2C-79.995&sort=best-match-asc&skip=0&limit=10&user_key=${apiKey}`;
+        console.log(url);
         request.onload = function() {
           if (this.status === 200) {
             resolve(request.response);
@@ -52,14 +52,18 @@ export class Search {
         request.send();
       });
 
-      name.then(function(response) {
-        let body = JSON.parse(response);
-        // $('#result').append(`${body.word} `);
-        return response;
-      }, function(error) {
-       // $('.showErrors').text(`There was an error processing your request: ${error.message}`);
-       return `There was an error processing your request: ${error.message}`;
-      });
+      return name;
+      // 
+      // name.then(function(response) {
+      //   // console.log(response);
+      //   let body = JSON.parse(response);
+      //   console.log(body.data[0].profile.first_name);
+      //   // $('#result').append(`${body.word} `);
+      //   return body;
+      // }, function(error) {
+      //  // $('.showErrors').text(`There was an error processing your request: ${error.message}`);
+      //  return `There was an error processing your request: ${error.message}`;
+      // });
     } else {
       return 'Something has gone horribly wrong!';
     }
