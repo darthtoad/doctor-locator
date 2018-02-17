d0ef54424f4e443dba0f4ba41d8e87287add0a5a
Sam Gespass <30681859+darthtoad@users.noreply.github.com>
2018-02-16 18:19:28 -0800

    catch empty results

--- js/scripts-interface.js
+++ js/scripts-interface.js
@@ -3,43 +3,49 @@ import { Search } from "./../js/scripts.js"
 Search.prototype.parseDataForIssue = function(issue) {
   issue.then(function(response){
     let body = JSON.parse(response);
-    for (let i = 0; i < body.data.length; i++) {
-      let profile = body.data[i].profile;
-      $("#result").append(`<hr><h3>Name: ${profile.first_name} ${profile.last_name}</h3>`)
-      for (let j = 0; j < body.data[i].practices.length; j++) {
-        $("#result").append(`<br><h5>Practice: ${body.data[i].practices[j].name}</h5><br>Accepting new patients: ${body.data[i].practices[j].accepts_new_patients}<br>${body.data[i].practices[j].visit_address.street}<br>${body.data[i].practices[j].visit_address.city}, ${body.data[i].practices[j].visit_address.state} ${body.data[i].practices[j].visit_address.zip}<br>`);
-        if (body.data[i].practices[j].website) {
-          $("#result").append(`Website: <a href="${body.data[i].practices[j].website}">Click Here</a><br>`);
+    if (body.data.length === 0) {
+      $("#result").append("Your search didn't yield any results. Try to rephrase your search.")
+    } else {
+      for (let i = 0; i < body.data.length; i++) {
+        let profile = body.data[i].profile;
+        $("#result").append(`<hr><h3>Name: ${profile.first_name} ${profile.last_name}</h3>`)
+        for (let j = 0; j < body.data[i].practices.length; j++) {
+          $("#result").append(`<br><h5>Practice: ${body.data[i].practices[j].name}</h5><br>Accepting new patients: ${body.data[i].practices[j].accepts_new_patients}<br>${body.data[i].practices[j].visit_address.street}<br>${body.data[i].practices[j].visit_address.city}, ${body.data[i].practices[j].visit_address.state} ${body.data[i].practices[j].visit_address.zip}<br>`);
+          if (body.data[i].practices[j].website) {
+            $("#result").append(`Website: <a href="${body.data[i].practices[j].website}">Click Here</a><br>`);
+          }
+          body.data[i].practices[j].phones.forEach(function(phone){
+            $("#result").append(`${phone.type}: ${phone.number}<br>`);
+          })
         }
-        body.data[i].practices[j].phones.forEach(function(phone){
-          $("#result").append(`${phone.type}: ${phone.number}<br>`);
-        })
       }
+    }}, function(error) {
+      $("#result").append(`There was an error processing your request: ${error.message}`);
     }
-  }, function(error) {
-   return `There was an error processing your request: ${error.message}`;
-  }
-);
+  );
 }
 
 Search.prototype.parseDataForName = function(name) {
   name.then(function(response) {
     let body = JSON.parse(response);
-    for (let i = 0; i < body.data.length; i++) {
-      let profile = body.data[i].profile;
-      $("#result").append(`<hr><h3>Name: ${profile.first_name} ${profile.last_name}</h3>`)
-      for (let j = 0; j < body.data[i].practices.length; j++) {
-        $("#result").append(`<br><h5>Practice: ${body.data[i].practices[j].name}</h5><br>Accepting new patients: ${body.data[i].practices[j].accepts_new_patients}<br>${body.data[i].practices[j].visit_address.street}<br>${body.data[i].practices[j].visit_address.city}, ${body.data[i].practices[j].visit_address.state} ${body.data[i].practices[j].visit_address.zip}<br>`);
-        if (body.data[i].practices[j].website) {
-          $("#result").append(`Website: <a href="${body.data[i].practices[j].website}">Click Here</a><br>`);
+    if (body.data.length === 0) {
+      $("#result").append("Your search didn't yield any results. Try to rephrase your search.")
+    } else {
+      for (let i = 0; i < body.data.length; i++) {
+        let profile = body.data[i].profile;
+        $("#result").append(`<hr><h3>Name: ${profile.first_name} ${profile.last_name}</h3>`)
+        for (let j = 0; j < body.data[i].practices.length; j++) {
+          $("#result").append(`<br><h5>Practice: ${body.data[i].practices[j].name}</h5><br>Accepting new patients: ${body.data[i].practices[j].accepts_new_patients}<br>${body.data[i].practices[j].visit_address.street}<br>${body.data[i].practices[j].visit_address.city}, ${body.data[i].practices[j].visit_address.state} ${body.data[i].practices[j].visit_address.zip}<br>`);
+          if (body.data[i].practices[j].website) {
+            $("#result").append(`Website: <a href="${body.data[i].practices[j].website}">Click Here</a><br>`);
+          }
+          body.data[i].practices[j].phones.forEach(function(phone){
+            $("#result").append(`${phone.type}: ${phone.number}<br>`);
+          })
         }
-        body.data[i].practices[j].phones.forEach(function(phone){
-          $("#result").append(`${phone.type}: ${phone.number}<br>`);
-        })
       }
-    }
-  }, function(error) {
-   return `There was an error processing your request: ${error.message}`;
+    }}, function(error) {
+   $("#result").append(`There was an error processing your request: ${error.message}`);
   });
 }
 
@@ -50,7 +56,11 @@ $(document).ready(function(){
     let name = $("#name").val();
     let numberOfResults = parseInt($("#results-name").val());
     let search = new Search("name", name, "best-match", numberOfResults);
-    search.parseDataForName(search.search());
+    if ($.isEmptyObject(search)) {
+      $("#result").append("Sorry, your search yielded no results. Try rephrasing your request.")
+    } else {
+      search.parseDataForName(search.search());
+    }
   })
 
   $("#by-issue").submit(function(event){
